cfc1740676f836403a6d8179bd166298e96a09ad
meeraramachandran <meeraramachandran@users.noreply.github.com>
2022-02-21 20:49:31 -0500

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -35,7 +35,7 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
+    "The track's approximate length to complete, in seconds"
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
